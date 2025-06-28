2e1fbec5a0b11023ad896ef5a82c6278c44ea64a
Sanket Patel <117096677+simpletire-sanket@users.noreply.github.com>
2025-06-28 20:09:07 +0530

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        Sanket's Personal Drive
+        {"Sanket's Personal Drive"}
       </div>
     </main>
   );
